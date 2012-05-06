4a894273b11fe0a9199c5c96071abb16b7d4660d
Karl Martino <kmartino@gmail.com>
2012-05-06 12:58:17 -0400

    Added test for parsing the Google Fusion SQL call to get a table id.

--- moneytalks_test.py
+++ moneytalks_test.py
@@ -2,7 +2,7 @@ import unittest
 import logging
 import sys
 
-from mock import MagicMock
+from minimock import Mock
 
 import moneytalks
 import moneytalks_config
@@ -25,20 +25,35 @@ class MoneyTalksTest(unittest.TestCase):
         self.assertIsNotNone(moneytalks_config.SOURCE)
 
     def test_create_table_sql(self):
-        table_name = 'contrib'
-        table_schema = ['ID', 'YEAR', 'CYCLE', 'CODE', 
+        fake_table_name = 'contrib'
+        fake_table_schema = ['ID', 'YEAR', 'CYCLE', 'CODE', 
                     'CONTRIBUTOR_NAME']
-        sql = fusion.create_table_sql(table_name, table_schema)
+        sql = fusion.create_table_sql(fake_table_name, fake_table_schema)
         expected_sql = '''CREATE TABLE 'contrib' (ID: STRING, YEAR: STRING, CYCLE: STRING, CODE: STRING, CONTRIBUTOR_NAME: STRING)'''
         self.assertEquals(expected_sql, sql)
 
     def test_insert_row_sql(self):
-        table_id = '123456'
-        row = {'ID':'123', 'YEAR':'2011', 'CYCLE':'1'}
-        sql = fusion.insert_row_sql(table_id, row)
+
+        fake_table_id = '123456'
+        fake_row = {'ID':'123', 'YEAR':'2011', 'CYCLE':'1'}
+        sql = fusion.insert_row_sql(fake_table_id, fake_row)
         expected_sql = '''INSERT INTO 123456 (CYCLE, ID, YEAR) VALUES ('1', '123', '2011')'''
         self.assertEquals(expected_sql, sql)
 
+    def test_get_table_id(self):
+
+        # set up mock results from Google Fusion SQL
+        fusion.send_query = Mock('fusion.send_query')
+        fusion.send_query.mock_returns = [['123','table1'], 
+                                          ['245','contrib']]
+        expected_table_id = '245'
+
+        fake_table_name ='contrib'
+        fake_auth_token = '123'
+        table_id = fusion.get_table_id(fake_table_name, fake_auth_token)
+        self.assertEquals(expected_table_id, table_id)
+
+        
 if '__main__' == __name__:
     unittest.main()
 
